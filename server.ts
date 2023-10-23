31a2ef8e4b7d62a7699caa2633989eb830ec8a98
AlmasN <95028680+Exiliahs@users.noreply.github.com>
2023-10-23 15:50:25 +0600

    added DB configuration and connection added products model

--- server.ts
+++ server.ts
@@ -6,10 +6,12 @@ import ordersRouter from './src/ordersService/orders';
 import userDataRouter from './src/userService/userData';
 import authControllerRouter from './src/authService/authController';
 import cartRouter from './src/cartService/cartService';
+import mongoose from 'mongoose';
+
 
 const app = express();
 const PORT = 3001;
-
+const DB_NAME = 'quick-shop-db-main';
 
 app.use(express.json());
 app.use(cors());
@@ -21,6 +23,15 @@ app.use('/api/products', productsRouter);
 app.use('/api/auth', authControllerRouter);
 app.use('/api/orders', ordersRouter);
 app.use('/api/cart', cartRouter);
+
+mongoose.connect(`mongodb://localhost:27017/${DB_NAME}`);
+
+export const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'Connection error:'));
+db.once('open', () => {
+    console.log('Connected to database');
+});
 
 
 app.listen(3001, () => {
